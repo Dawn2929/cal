13d78686d59f871d9a363f09b4a226e14ab8911f
Tong W <tongw@Tongs-MacBook-Pro.local>
2019-10-28 16:01:16 -0400

    test1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalComponent } from './cal/cal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalComponent
   ],
   imports: [
     BrowserModule
